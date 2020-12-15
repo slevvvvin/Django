a6d9f2c75c061f6b1b93cf3eceb6e706c1c05be6
slevvvvin <slevvvvin@gmail.com>
2020-12-15 16:21:22 +0200

    replace Date field with DateTime field and delete data_volumes from dockerfile

--- blog/models.py
+++ blog/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
     title_tag = models.CharField(max_length=250)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
-    post_date = models.DateField(auto_now_add=True)
+    post_date = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
