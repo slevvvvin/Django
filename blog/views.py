0af53f2d390a0bbf79f588f10ca2d3e681f6489a
slevvvvin <slevvvvin@gmail.com>
2020-12-16 15:38:53 +0200

    added custom template for categories

--- blog/views.py
+++ blog/views.py
@@ -12,12 +12,6 @@ class HomeView(ListView):
     template_name = 'home.html'
     ordering = ['-post_date']
 
-# TODO: move outside
-    def get_context_data(self, *args, **kwargs):
-        context = super(HomeView, self).get_context_data()
-        context["category_list"] = Category.objects.all()
-        return context
-
 
 class CategoryView(DetailView):
     model = Post
@@ -35,20 +29,10 @@ class UserRegisterView(generic.CreateView):
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(UserRegisterView, self).get_context_data()
-        context["category_list"] = Category.objects.all()
-        return context
-
 
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_details.html'
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(PostDetailView, self).get_context_data()
-        context["category_list"] = Category.objects.all()
-        return context
 
 
 class AddPostView(CreateView):
@@ -56,21 +40,11 @@ class AddPostView(CreateView):
     form_class = PostForm
     template_name = 'add_post.html'
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(AddPostView, self).get_context_data()
-        context["category_list"] = Category.objects.all()
-        return context
-
 
 class AddCategoryView(CreateView):
     model = Category
     template_name = 'add_category.html'
     fields = '__all__'
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(AddCategoryView, self).get_context_data()
-        context["category_list"] = Category.objects.all()
-        return context
 
 
 class UpdatePostView(UpdateView):
@@ -78,18 +52,8 @@ class UpdatePostView(UpdateView):
     form_class = EditForm
     template_name = 'update_post.html'
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(UpdatePostView, self).get_context_data()
-        context["category_list"] = Category.objects.all()
-        return context
-
 
 class DeletePostView(DeleteView):
     model = Post
     template_name = 'delete_post.html'
     success_url = reverse_lazy('home')
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(DeletePostView, self).get_context_data()
-        context["category_list"] = Category.objects.all()
-        return context
