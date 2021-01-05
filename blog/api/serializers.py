4c61088f675bb9afaef3a64d24c47200dcfbf09c
slevvvvin <slevvvvin@gmail.com>
2021-01-05 15:15:57 +0200

    added api views with using mixins

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from blog.models import Post, Category, Comment
+from blog.models import Post, Category
 
 
 class PostListSerializer(serializers.ModelSerializer):
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,85 +1,71 @@
-from django.http import Http404
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.views import APIView
 from .serializers import PostListSerializer, PostDetailSerializer, \
     CategoryListSerializer, CategoryDetailSerializer
-from blog.models import Post, Category, Comment
+from blog.models import Post, Category
+from rest_framework import mixins
+from rest_framework import generics
 
 
-class PostListAPIView(APIView):
-    @staticmethod
-    def get(request):
-        posts = Post.objects.all()
-        serializer = PostListSerializer(posts, many=True)
-        return Response(serializer.data)
+class PostListAPIView(mixins.ListModelMixin,
+                      mixins.CreateModelMixin,
+                      generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostListSerializer
 
-    @staticmethod
-    def post(request):
-        serializer = PostListSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class PostDetailAPIView(APIView):
-    @staticmethod
-    def get(request, pk):
-        post = Post.objects.get(id=pk)
-        serializer = PostDetailSerializer(post)
-        return Response(serializer.data)
+class PostDetailAPIView(mixins.RetrieveModelMixin,
+                        mixins.UpdateModelMixin,
+                        mixins.DestroyModelMixin,
+                        generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
 
-    @staticmethod
-    def put(request, pk):
-        post = Post.objects.get(id=pk)
-        serializer = PostDetailSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    @staticmethod
-    def delete(request, pk):
-        post = Post.objects.get(id=pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
 
-class CategoryListAPIView(APIView):
-    @staticmethod
-    def get(request):
-        categories = Category.objects.all()
-        serializer = CategoryListSerializer(categories, many=True)
-        return Response(serializer.data)
+class CategoryListAPIView(mixins.ListModelMixin,
+                          mixins.CreateModelMixin,
+                          generics.GenericAPIView):
 
-    @staticmethod
-    def post(request):
-        serializer = CategoryListSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Category.objects.all()
+    serializer_class = CategoryListSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class CategoryDetailAPIView(APIView):
+class CategoryDetailAPIView(mixins.RetrieveModelMixin,
+                            mixins.UpdateModelMixin,
+                            mixins.DestroyModelMixin,
+                            generics.GenericAPIView):
+
+    queryset = Category.objects.all()
+    serializer_class = CategoryListSerializer
+
     @staticmethod
     def get(request, pk):
         posts = Post.objects.filter(category_id=pk)
         serializer = PostListSerializer(posts, many=True)
         return Response(serializer.data)
 
-    @staticmethod
-    def put(request, pk):
-        category = Category.objects.get(id=pk)
-        serializer = CategoryListSerializer(category, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    @staticmethod
-    def delete(request, pk):
-        snippet = Category.objects.get(id=pk)
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
