cbf07d6ce84461f76e0729b6a7a73bb82bcfb761
slevvvvin <slevvvvin@gmail.com>
2021-01-14 17:14:37 +0200

    implemented ability only for admin to create, delete or updete posts and categories

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
 from .views import (PostListAPIView, PostDetailAPIView, CategoryListAPIView,
-                    CategoryDetailAPIView, PostListByCategory)
+                    CategoryDetailAPIView, PostListByCategory,
+                    PostCreateAPIView, CategoryCreateAPIView,
+                    PostPutDeleteAPIView, CategoryPutDeleteAPIView)
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('posts/', PostListAPIView.as_view()),
     path('posts/<int:pk>', PostDetailAPIView.as_view()),
-    path('categories/', CategoryListAPIView.as_view()),
-    path('categories/<int:pk>', CategoryDetailAPIView.as_view()),
+    path('posts/create', PostCreateAPIView.as_view()),
+    path('posts/put_delete/<int:pk>', PostPutDeleteAPIView.as_view()),
     path('posts/category/<int:pk>', PostListByCategory.as_view()),
+    path('categories/', CategoryListAPIView.as_view()),
+    path('categories/create', CategoryCreateAPIView.as_view()),
+    path('categories/<int:pk>', CategoryDetailAPIView.as_view()),
+    path('categories/put_delete/<int:pk>', CategoryPutDeleteAPIView.as_view()),
     path('token', TokenObtainPairView.as_view()),
     path('token/refresh', TokenRefreshView.as_view()),
 ]
--- blog/api/views.py
+++ blog/api/views.py
@@ -4,18 +4,47 @@ from blog.models import Post, Category
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from rest_framework import mixins
 
 
-class PostListAPIView(generics.ListCreateAPIView):
+class PostListAPIView(mixins.ListModelMixin, generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostListSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class PostDetailAPIView(mixins.RetrieveModelMixin, generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+
+class PostCreateAPIView(mixins.CreateModelMixin, generics.GenericAPIView):
     queryset = Post.objects.all()
     serializer_class = PostListSerializer
     permission_classes = [IsAdminUser]
 
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
-class PostDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+
+class PostPutDeleteAPIView(mixins.UpdateModelMixin,
+                           mixins.DestroyModelMixin,
+                           generics.GenericAPIView):
     queryset = Post.objects.all()
     serializer_class = PostDetailSerializer
+    permission_classes = [IsAdminUser]
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
 
 class PostListByCategory(APIView):
@@ -26,13 +55,40 @@ class PostListByCategory(APIView):
         return Response(serializer.data)
 
 
-class CategoryListAPIView(generics.ListCreateAPIView):
-
+class CategoryListAPIView(mixins.ListModelMixin, generics.GenericAPIView):
     queryset = Category.objects.all()
     serializer_class = CategoryListSerializer
 
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-class CategoryDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
 
+class CategoryDetailAPIView(mixins.RetrieveModelMixin, generics.GenericAPIView):
     queryset = Category.objects.all()
     serializer_class = CategoryListSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+
+class CategoryCreateAPIView(mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategoryListSerializer
+    permission_classes = [IsAdminUser]
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class CategoryPutDeleteAPIView(mixins.UpdateModelMixin,
+                               mixins.DestroyModelMixin,
+                               generics.GenericAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategoryListSerializer
+    permission_classes = [IsAdminUser]
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
