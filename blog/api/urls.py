017b12c9694bf847b468222009883ce55cde1ac2
slevvvvin <slevvvvin@gmail.com>
2021-01-18 18:28:56 +0200

    replace views with views set for api and fix urls and serializers, also use custom permission for views

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,19 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.routers import DefaultRouter
 from blog.api import views
 
+router = DefaultRouter()
+router.register('posts', views.PostView)
+router.register('categories', views.CategoryView)
+
 urlpatterns = [
-    path('posts/', views.PostListView.as_view()),
-    path('posts/<int:pk>', views.PostDetailView.as_view()),
-    path('posts/new', views.PostCreateView.as_view()),
-    path('posts/<int:pk>/edit', views.PostUpdateView.as_view()),
-    path('posts/<int:pk>/delete', views.PostDeleteView.as_view()),
     path('posts/category/<int:pk>', views.PostListByCategory.as_view()),
-    path('categories/', views.CategoryListView.as_view()),
-    path('categories/new', views.CategoryCreateView.as_view()),
-    path('categories/<int:pk>', views.CategoryDetailView.as_view()),
-    path('categories/<int:pk>/edit', views.CategoryUpdateView.as_view()),
-    path('categories/<int:pk>/delete', views.CategoryDeleteView.as_view()),
     path('token', TokenObtainPairView.as_view()),
     path('token/refresh', TokenRefreshView.as_view()),
+    path('', include(router.urls)),
 ]
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,102 +1,29 @@
-from rest_framework import generics, mixins
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import permissions
+from rest_framework import viewsets
 from blog.api import serializers
 from blog.models import Post, Category
+from blog.api import permission
 
 
-class PostListView(mixins.ListModelMixin, generics.GenericAPIView):
+class PostView(viewsets.ModelViewSet):
+
     queryset = Post.objects.all()
-    serializer_class = serializers.PostListSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-
-class PostDetailView(mixins.RetrieveModelMixin, generics.GenericAPIView):
-    queryset = Post.objects.all()
-    serializer_class = serializers.PostDetailSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-
-class PostCreateView(mixins.CreateModelMixin, generics.GenericAPIView):
-    queryset = Post.objects.all()
-    serializer_class = serializers.PostListSerializer
-    permission_classes = [permissions.IsAdminUser]
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class PostUpdateView(mixins.UpdateModelMixin, generics.GenericAPIView):
-    queryset = Post.objects.all()
-    serializer_class = serializers.PostDetailSerializer
-    permission_classes = [permissions.IsAdminUser]
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-
-class PostDeleteView(mixins.DestroyModelMixin,
-                     generics.GenericAPIView):
-    queryset = Post.objects.all()
-    serializer_class = serializers.PostDetailSerializer
-    permission_classes = [permissions.IsAdminUser]
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+    serializer_class = serializers.PostSerializer
+    permission_classes = [permission.IsAdminUserOrReadOnly]
 
 
 class PostListByCategory(APIView):
+
     @staticmethod
     def get(request, pk):
         posts = Post.objects.filter(category_id=pk)
-        serializer = serializers.PostListSerializer(posts, many=True)
+        serializer = serializers.PostSerializer(posts, many=True)
         return Response(serializer.data)
 
 
-class CategoryListView(mixins.ListModelMixin, generics.GenericAPIView):
+class CategoryView(viewsets.ModelViewSet):
+
     queryset = Category.objects.all()
-    serializer_class = serializers.CategoryListSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-
-class CategoryDetailView(mixins.RetrieveModelMixin, generics.GenericAPIView):
-    queryset = Category.objects.all()
-    serializer_class = serializers.CategoryListSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-
-class CategoryCreateView(mixins.CreateModelMixin, generics.GenericAPIView):
-    queryset = Category.objects.all()
-    serializer_class = serializers.CategoryListSerializer
-    permission_classes = [permissions.IsAdminUser]
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class CategoryUpdateView(mixins.UpdateModelMixin, generics.GenericAPIView):
-    queryset = Category.objects.all()
-    serializer_class = serializers.CategoryListSerializer
-    permission_classes = [permissions.IsAdminUser]
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-
-class CategoryDeleteView(mixins.DestroyModelMixin,
-                         generics.GenericAPIView):
-    queryset = Category.objects.all()
-    serializer_class = serializers.CategoryListSerializer
-    permission_classes = [permissions.IsAdminUser]
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+    serializer_class = serializers.CategorySerializer
+    permission_classes = [permission.IsAdminUserOrReadOnly]
